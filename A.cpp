7b525b48745a8182a4bffb37aa140b8433796e4d
aditya.raghav <raghav.placement1822@gmail.com>
2025-03-31 17:22:14 +0530

    some refactoring

--- A.cpp
+++ A.cpp
@@ -7,7 +7,16 @@ using namespace std;
 #define int long long 
 
 void solve(){
-    return ;
+
+    vector<int>A(12,0); 
+    iota( A.begin() , A.end() , 1 );
+
+    rotate( A.begin() , A.begin()+2 , A.end() );
+    for( auto x : A )
+      cout<<x<<",";
+
+    cout<<endl;
+
 }
 
 int32_t main() {
@@ -15,4 +24,24 @@ int32_t main() {
   solve();
 
 return 0;
-}   
+}    
+
+
+/*
+  
+  Prateek Downstream wala task, 
+  Is week dev complete krk QA me move krdenge,
+  on track for this task
+
+ [a,b] [c,d]
+
+ |a-c| + |b-d|
+
+ -> a - c + b - d ->  a+b - ( c+d )
+ -> a - c - b + d ->  a-c - ( c-d )
+
+*/
+
+
+
+
